e37102fb3e4f6b71d73abebbd5a74c51002f6a7f
TJ-Rocke <spook.devv@gmail.com>
2025-05-18 21:55:26 -0400

    Defining Component Prop types

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,9 +1,15 @@
-export default function (props: ) {
+export default function ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
       <button>Delete</button>
     </article>
